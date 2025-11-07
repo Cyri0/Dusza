0541ef9e5dd85c4e00c3e55b0dda2aceb9f8b765
al144 <davidasztral2007@gmail.com>
2025-11-07 13:34:04 +0100

    base+

--- jatek/jatek/settings.py
+++ jatek/jatek/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yd)s0r$q*mmkvg@9kjd!#1dme9rgzm+q)yq!8$s+vka1sqwg0w'
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jatek',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,11 @@ ROOT_URLCONF = 'jatek.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'sites',
+            BASE_DIR / 'views',
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
