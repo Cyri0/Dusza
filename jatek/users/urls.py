a1501f8cb5454e3a99f093f5e278f3e6f2ef8c7f
Haller2000 <hallerpeter7@gmail.com>
2025-11-08 14:32:41 +0100

    userskesz

--- jatek/users/urls.py
+++ jatek/users/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('player/login/', views.player_login, name='player_login'),
     path('gamemaster/login/', views.gamemaster_login, name='gamemaster_login'),
     path('player/dashboard/', views.player_dashboard, name='player_dashboard'),
-    path('gamemaster/dashboard/', views.gamemaster_dashboard, name='gamemaster_dashboard'),
     path('logout/', views.logout, name='logout'),
 ]
